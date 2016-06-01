aa539da6908e5a94084380d8f1038b8eb40faa40
Jefrien Alvizures <jefrienalvizures@gmail.com>
2016-06-01 00:19:18 -0600

    Diseño...

--- app/src/main/java/org/jefrienalvizures/clashtournament/fragments/InicioFragment.java
+++ app/src/main/java/org/jefrienalvizures/clashtournament/fragments/InicioFragment.java
@@ -7,16 +7,50 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.TextView;
 
 import org.jefrienalvizures.clashtournament.R;
+import org.jefrienalvizures.clashtournament.bean.Usuario;
+import org.jefrienalvizures.clashtournament.clases.Comunicador;
 
 /**
  * Created by Familia on 31/05/2016.
  */
 public class InicioFragment extends Fragment {
+
+    TextView usuarioTxt;
+    WebView wb;
+    Usuario u;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_inicio,container,false);
+        u = Comunicador.getUsuario();
+        View v;
+        if(u.getClan() == 0){
+             v = inflater.inflate(R.layout.fragment_inicio,container,false);
+        } else {
+            v = inflater.inflate(R.layout.fragment_inicio0,container,false);
+        }
+
+
+
+        usuarioTxt = (TextView) v.findViewById(R.id.usuarioTxtInicio);
+        usuarioTxt.setText(u.getNombre());
+
+        Descripcion(v);
+
+        return v;
     }
+
+    public void Descripcion(View v){
+        wb= (WebView) v.findViewById(R.id.webViewDesc);
+        String text;
+        text = "<html<body><p align=\"justify\">";
+        text+= getString(R.string.bienvenida);
+        text+= "</p></body></html>";
+        wb.loadData(text, "text/html", "utf-8");
+    }
+
 }
